f3a927ade3b7af1ad1861ef1f9ef1f4c3fbaf4f5
Shantanu Tiwari <shantanutiwari240704@gmail.com>
2024-12-29 19:12:41 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,11 @@
 import { Navbar } from "@/components/Navbar";
-import { Hero } from "@/components/Hero";
-import { Stack } from "@/components/Stack";
-import {Portfolio} from "@/components/Portfolio";
-import {KeyMetrics} from "@/components/KeyMetrics";
-import {Services} from "@/components/Services";
-import {Contact} from "@/components/Contact";
-import {Footer} from "@/components/Footer";
+
 
 export default function Home() {
     return (
         <>
             <Navbar />
-            <Hero />
-            <Stack />
-            <Portfolio/>
-            <KeyMetrics/>
-            <Services/>
-            <Contact/>
-            <Footer/>
+
         </>
     );
 }
