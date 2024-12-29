8123c34529646ce09c084cf9503fc9fe89b15ecf
Shantanu Tiwari <shantanutiwari240704@gmail.com>
2024-12-29 19:02:01 +0530

    Update Portfolio.tsx

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -1,10 +1,10 @@
 "use client";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import project1 from "@/assets/proj2.png";
 import project2 from "@/assets/proj1.png";
 import project3 from "@/assets/proj3.png";
-import {motion, useMotionTemplate, useMotionValue, animate} from "framer-motion";
+import { motion, useMotionTemplate, useMotionValue, animate } from "framer-motion";
 
 const projects = [
     {
@@ -32,33 +32,34 @@ const projects = [
         image: project3,
     },
 ];
+
 const COLORS_TOP = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"];
-
-
-
-
-
 
 export const Portfolio = () => {
     const [selectedProject, setSelectedProject] = useState(projects[0]);
     const color = useMotionValue(COLORS_TOP[0]);
 
     useEffect(() => {
-        animate(color, COLORS_TOP, {
-            ease: "easeInOut",
-            duration: 10,
-            repeat: Infinity,
-            repeatType: "mirror",
-        });
+        if (typeof window !== "undefined") {
+            animate(color, COLORS_TOP, {
+                ease: "easeInOut",
+                duration: 10,
+                repeat: Infinity,
+                repeatType: "mirror",
+            });
+        }
     }, [color]);
+
     const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #000 50%, ${color})`;
 
     return (
-        <motion.section style={{
-            backgroundImage
-        }}
-                        id="portfolio"
-                        className="py-32 text-white">
+        <motion.section
+            style={{
+                backgroundImage,
+            }}
+            id="portfolio"
+            className="py-32 text-white"
+        >
             <div className="max-w-7xl mx-auto px-4 grid lg:grid-cols-2 gap-12 items-center">
                 {/* Left Section: Project Details */}
                 <div>
