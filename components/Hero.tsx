781ad7e3b6045a7bd878ebf570f69a6dddeddd0c
Shantanu Tiwari <shantanutiwari240704@gmail.com>
2024-12-30 12:16:08 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,21 +21,22 @@ export const Hero = () => {
     }, [color]);
 
     const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #000 50%, ${color})`;
- 
 
     return (
         <motion.section
             style={{
                 backgroundImage,
             }}
-            className="relative grid min-h-screen place-content-center overflow-hidden px-4 py-24 text-gray-200"
+            className="relative grid min-h-screen place-content-center overflow-hidden px-4 py-24 text-gray-200 text-center"
         >
             <div className="z-10 flex flex-col items-center">
                 <span className="mb-1.5 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 text-sm">
                     Open for work
                 </span>
-                <h1 className="text-white/40 text-7xl font-black">Hi, I am</h1>
-                <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text font-black leading-tight text-transparent text-5xl md:text-7xl lg:text-8xl">
+                <h1 className="text-white/40 text-3xl sm:text-5xl md:text-7xl lg:text-8xl font-black">
+                    Hi, I am
+                </h1>
+                <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text font-black leading-tight text-transparent text-3xl sm:text-5xl md:text-7xl lg:text-8xl">
                     Shantanu Tiwari
                 </h1>
                 <div className="flex flex-col items-center">
@@ -43,7 +44,7 @@ export const Hero = () => {
                         src={profilepic}
                         alt="Profile picture of Shantanu Tiwari"
                         width={250}
-                        className="rounded-full"
+                        className="rounded-full mx-auto"
                     />
                     <motion.a
                         href="https://github.com/Shantanu-Tiwari"
